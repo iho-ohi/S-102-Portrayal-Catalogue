8a0a80c16dde78dacf9e6a2c17bcbe794f861e6a
mikan66 <4913543+mikan66@users.noreply.github.com>
2024-07-22 18:02:56 -0400

    #5, Lua rule for QualityOfBathymetryCoverage

---
+++ Lua/Rules/QualityOfBathymetryCoverage.lua
@@ -0,0 +1,7 @@
+function QualityOfBathymetryCoverage(feature, featurePortrayal, contextParameters)
+	
+	featurePortrayal:AddInstructions('ViewingGroup:13030;DrawingPriority:3;DisplayPlane:UnderRadar;NullInstruction')
+
+
+	return viewingGroup
+end
