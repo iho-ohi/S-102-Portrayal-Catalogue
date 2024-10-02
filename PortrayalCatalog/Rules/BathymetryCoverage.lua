3107969974eec94926457f4e8ec1ebd00db0b44f
DavidGrant-NIWC <62906211+DavidGrant-NIWC@users.noreply.github.com>
2024-10-02 14:04:41 -0400

    Fixes #21

--- PortrayalCatalog/Rules/BathymetryCoverage.lua
+++ PortrayalCatalog/Rules/BathymetryCoverage.lua
@@ -6,12 +6,13 @@ function BathymetryCoverage(feature, featurePortrayal, contextParameters)
 	
 	featurePortrayal:AddInstructions('CoverageColor:DEPIT,0;LookupEntry:Intertidal,,0,ltSemiInterval')
 	if contextParameters.FourShades then
-		featurePortrayal:AddInstructions('CoverageColor:DEPVS,0;LookupEntry:Shallow Water,0,' .. tostring(contextParameters.ShallowContour) .. ',geLtInterval;CoverageFill:depth')
+		featurePortrayal:AddInstructions('CoverageColor:DEPVS,0;LookupEntry:Shallow Water,0,' .. tostring(contextParameters.ShallowContour) .. ',geLtInterval')
 		featurePortrayal:AddInstructions('CoverageColor:DEPMS,0;LookupEntry:Medium-Shallow Water,' .. tostring(contextParameters.ShallowContour) .. ',' .. tostring(contextParameters.SafetyContour) .. ',geLtInterval')
 		featurePortrayal:AddInstructions('CoverageColor:DEPMD,0;LookupEntry:Medium-Deep Water,' .. tostring(contextParameters.SafetyContour) .. ',' .. tostring(contextParameters.DeepContour) .. ',geLtInterval')
 		featurePortrayal:AddInstructions('CoverageColor:DEPDW,0;LookupEntry:Deep Water,'.. tostring(contextParameters.DeepContour) .. ',,geSemiInterval')
 	else
-		featurePortrayal:AddInstructions('CoverageColor:DEPVS,0;LookupEntry:Shallow Water,0,' .. tostring(contextParameters.SafetyContour) .. ',geLtInterval;CoverageFill:depth')
+		featurePortrayal:AddInstructions('CoverageColor:DEPVS,0;LookupEntry:Shallow Water,0,' .. tostring(contextParameters.SafetyContour) .. ',geLtInterval')
 		featurePortrayal:AddInstructions('CoverageColor:DEPDW,0;LookupEntry:Deep Water,'.. tostring(contextParameters.SafetyContour) .. ',,geSemiInterval')
 	end
+	featurePortrayal:AddInstructions('CoverageFill:depth')
 end
